116e7a8c42d36ecb8136f5a8d21e88148396ed56
IhsanBaihaqii <baihaqiihsan31@gmail.com>
2026-01-04 23:18:57 +0700

    Fix Structure Folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-import NavbarView from "./NavbarView";
+import Layout from "./Layout";
 
 function App() {
-  return <NavbarView />;
+  return <Layout />;
 }
 
 export default App;
---
+++ src/Layout/index.jsx
@@ -0,0 +1,11 @@
+import NavbarView from "./NavbarView";
+
+function MainLayout() {
+  return (
+    <div className="bg-[#0a0a1a] h-screen ">
+      <NavbarView />
+    </div>
+  );
+}
+
+export default MainLayout;
