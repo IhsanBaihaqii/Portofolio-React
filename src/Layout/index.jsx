07f7c0bffd5c44a1d0a39464f34742672457390c
IhsanBaihaqii <baihaqiihsan31@gmail.com>
2026-01-18 17:50:52 +0700

    rename ProjectView

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -2,7 +2,7 @@ import NavbarView from "./NavbarView";
 import HomeView from "./HomeView";
 import AboutMeView from "./AboutMeView";
 import SkillView from "./SkillView";
-import ProjectView from "./Projects";
+import ProjectView from "./ProjectView";
 import EducationView from "./EducationView";
 
 function MainLayout() {
