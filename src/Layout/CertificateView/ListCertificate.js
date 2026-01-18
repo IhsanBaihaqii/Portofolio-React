d6bfba90e8130312cb5dfc811a04e3be388e5315
IhsanBaihaqii <baihaqiihsan31@gmail.com>
2026-01-18 18:33:47 +0700

    update ListCertificate (add Intermediate MySQL)

--- src/Layout/CertificateView/ListCertificate.js
+++ src/Layout/CertificateView/ListCertificate.js
@@ -7,10 +7,10 @@ export default [
     tahun: "28 Mei 2025",
   },
   {
-    title: "Introduction MySQL",
-    description: "Solo Learn | Introduction MySQL",
+    title: "Intermediate MySQL",
+    description: "Solo Learn | Intermediate MySQL",
     image:
-      "https://api2.sololearn.com/v2/certificates/CC-EBHMMIJH/image/png?t=638839987292660800",
-    tahun: "28 Mei 2025",
+      "https://api2.sololearn.com/v2/certificates/CC-QESVGTAL/image/png?t=638851371745931050",
+    tahun: "10 Juni 2025",
   },
 ];
